abffbe0c9bb7f71d51c08ca748590141e75912f8
Björn Ritzl <bjorn.ritzl@gmail.com>
2018-05-17 08:42:02 +0200

    Initial commit

---
+++ example/thing.go
@@ -0,0 +1,68 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/example/assets/example.atlas\"\n"
+  "default_animation: \"icon_72\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 1.0\n"
+  "friction: 0.0\n"
+  "restitution: 1.0\n"
+  "group: \"thing\"\n"
+  "mask: \"thing\"\n"
+  "mask: \"wall\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.38268343\n"
+  "      w: 0.9238795\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 25.0\n"
+  "  data: 25.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ metrics/fps.lua
@@ -0,0 +1,54 @@
+local M = {}
+
+M.POSITION = vmath.vector3(10, 20, 0)
+M.FORMAT = "FPS %.2f"
+
+function M.create()
+	local instance = {}
+
+	local frames = {}
+
+	local fps = 0
+
+	function instance.update()
+		table.insert(frames, socket.gettime())
+		if #frames == 61 then
+			table.remove(frames, 1)
+			fps = 1 / ((frames[#frames] - frames[1]) / (#frames - 1))
+		end
+	end
+
+	function instance.fps()
+		return fps
+	end
+
+	function instance.draw(format, position)
+		format = format or M.FORMAT
+		position = position or M.POSITION
+		msg.post("@render:", "draw_text", { text = format:format(fps), position = position } )
+	end
+
+	return instance
+end
+
+
+local singleton = M.create()
+
+
+function M.update()
+	singleton.update()
+end
+
+
+function M.fps()
+	return singleton.fps()
+end
+
+
+function M.draw(format, position)
+	singleton.draw(format, position)
+end
+
+
+
+return M
